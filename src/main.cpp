2e74e44e19f89df0b6d21c9ec9e3e1802bf69aba
raineio <64136899+raineio@users.noreply.github.com>
2022-07-14 05:29:04 +0200

    Add project files

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include "main.hpp"
+
+static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
+
+// Loads the config from disk using our modInfo, then returns it for use
+// other config tools such as config-utils don't use this config, so it can be removed if those are in use
+Configuration& getConfig() {
+    static Configuration config(modInfo);
+    return config;
+}
+
+// Returns a logger, useful for printing debug messages
+Logger& getLogger() {
+    static Logger* logger = new Logger(modInfo);
+    return *logger;
+}
+
+// Called at the early stages of game loading
+extern "C" void setup(ModInfo& info) {
+    info.id = MOD_ID;
+    info.version = VERSION;
+    modInfo = info;
+	
+    getConfig().Load();
+    getLogger().info("Completed setup!");
+}
+
+// Called later on in the game loading - a good time to install function hooks
+extern "C" void load() {
+    il2cpp_functions::Init();
+
+    getLogger().info("Installing hooks...");
+}
